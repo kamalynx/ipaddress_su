7acce76fbb0e9ca6ebbd6ea84bb6f89d3f973bb8
kamalynx <kamalynxpro@gmail.com>
2025-04-08 21:30:41 +0500

    add sitemap.xml 2

---
+++ articles/sitemaps.py
@@ -0,0 +1,13 @@
+from django.contrib.sitemaps import Sitemap
+
+from articles.models import Article
+
+
+class ArticlesSitemap(Sitemap):
+    priority = 0.5
+
+    def items(self):
+        return Article.objects.filter(published=True).all()
+
+    def lastmod(self, obj):
+        return obj.created_at
